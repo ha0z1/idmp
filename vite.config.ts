3785ab74ae524d09625f3ebec265cfdb7d0f9ea6
haozi <syntaxright@gmail.com>
2023-10-31 22:34:29 +0800

    feat: 🎸 first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), dts()],
+    build: {
+      // minify: !false,
+      // esbuild:
+      //   mode !== 'development'
+      //     ? {
+      //         drop: ['console', 'debugger'],
+      //       }
+      //     : null,
+      lib: {
+        formats: ['es'],
+        entry: './src/index.ts',
+        name: 'idmp',
+        fileName: 'index',
+      },
+    },
+    define: {
+      // 'process.env.NODE_ENV': JSON.stringify('production'),
+      // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    },
+  }
+})
