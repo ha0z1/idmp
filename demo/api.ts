346b6780f1491327ff2e73c5535e3e4d0d15ff8e
haozi <syntaxright@gmail.com>
2023-10-31 22:34:29 +0800

    feat: 🎸 first commit

---
+++ demo/api.ts
@@ -0,0 +1,21 @@
+import idmp, { g } from '../src'
+;(window as any).idmp = idmp
+;(window as any).g = g
+// export const getUserData = async (userId: string) => {
+//   const API = `http://127.0.0.1:3000/api/user-info?id=${userId}&t=${Math.random()}`
+//   const res: { id: string } = await fetch(API).then((d) => d.json())
+//   return res
+// }
+
+export const getUserData = async (userId: string) => {
+  const API = `https://haozi.me/?id=${userId}&t=${Math.random()}`
+  await fetch(API).then((d) => d.text())
+
+  const res = { id: userId, val: Math.random() }
+  return res
+}
+
+export const getUserDataIdmp = (userId: string) => {
+  const key = `getUserData:${userId}`
+  return idmp(key, () => getUserData(userId))
+}
---
+++ mock/api.ts
@@ -0,0 +1,26 @@
+import http from 'http'
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const sleep = (delay: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
+
+const server = http.createServer(async (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // await sleep(2000)
+  const id = new URL(`http://127.0.0.1${req.url}`).searchParams.get('id')
+  res.end(
+    JSON.stringify({
+      id,
+      val: Math.random(),
+    }),
+  )
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
