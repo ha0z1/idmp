5c03590aa12ef8fd4671d9e03b933b22e9ebca80
haozi <syntaxright@gmail.com>
2023-11-16 13:16:51 +0800

    test: 💍 Add unit test (#6)

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,7 +1,38 @@
 import React, { useState } from 'react'
 import Item from './Item'
 import ItemIdmp from './ItemIdmp'
+// import idmp from '../src'
 
+// const getInfo = async () => {
+//   // const API = `https://haozi.meaa/?api/your-info`
+//   const API = `http://127.0.0.1:3000/`
+//   return await fetch(API).then((d) => d.json())
+// }
+
+// // Only this line changed
+// export const getInfoIdmp = () =>
+//   idmp('/api/your-info', getInfo, { maxRetry: 50 , maxAge: 86400000})
+
+// for (let i = 0; i < 10; ++i) {
+//   getInfoIdmp()
+//     .then((d) => {
+//       console.log(111)
+//       // console.log(d)
+//     })
+//     .catch(() => {
+//       console.log(444)
+//     })
+// }
+// for (let i = 0; i < 10; ++i) {
+//   getInfoIdmp()
+//     .then((d) => {
+//       console.log(111)
+//       // console.log(d)
+//     })
+//     .catch(() => {
+//       console.log(444)
+//     })
+// }
 const list = [
   '123',
   '456',
