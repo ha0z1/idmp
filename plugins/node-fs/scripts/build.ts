0d41e9bfb52189a4ef68efae974d0459ce97b109
haozi <syntaxright@gmail.com>
2024-06-05 21:51:09 +0800

    feat: 🎸 plugins support cjs

--- plugins/node-fs/scripts/build.ts
+++ plugins/node-fs/scripts/build.ts
@@ -3,7 +3,7 @@ import { build, mergeConfig, type InlineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 import banner from 'vite-plugin-banner'
-import { version, dependencies } from '../package.json'
+import { version } from '../package.json'
 
 type DeepPartial<T> = T extends object
   ? {
@@ -15,18 +15,14 @@ const buildFile = async (buildOptions: DeepPartial<InlineConfig>) => {
     mergeConfig(
       {
         plugins: [
-          // react(),
+          react(),
           dts(),
           banner(`/*! idmp v${version} | (c) github/haozi | MIT */`),
         ],
         build: {
           sourcemap: false,
           // target: "chrome51",
-
-          target: 'node18',
-          rollupOptions: {
-            external: [/^node:.*/, 'os', 'fs', ...Object.keys(dependencies)],
-          },
+          target: 'es6',
           lib: {
             formats: ['es'],
             entry: 'src/index.ts',
@@ -41,6 +37,7 @@ const buildFile = async (buildOptions: DeepPartial<InlineConfig>) => {
 }
 ;(async () => {
   await Promise.all([
+    // base
     buildFile({
       build: {
         minify: false,
@@ -49,6 +46,59 @@ const buildFile = async (buildOptions: DeepPartial<InlineConfig>) => {
           formats: ['es'],
           fileName: () => 'index.js',
         },
+      },
+    }),
+
+    // browser
+    buildFile({
+      build: {
+        minify: true,
+        lib: {
+          formats: ['umd'],
+          fileName: () => 'index.browser.umd.js',
+        },
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+
+    // browser esm
+    buildFile({
+      build: {
+        minify: true,
+        lib: {
+          formats: ['es'],
+          fileName: () => 'index.browser.esm.js',
+        },
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+
+    // node.cjs
+    buildFile({
+      build: {
+        minify: false,
+        lib: {
+          formats: ['cjs'],
+          fileName: () => 'index.node.cjs',
+        },
+      },
+    }),
+
+    // deno
+    buildFile({
+      build: {
+        minify: false,
+        lib: {
+          formats: ['es'],
+          fileName: () => 'index.deno.js',
+        },
+      },
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('production'),
       },
     }),
   ])
