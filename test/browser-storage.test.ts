5915cdf5dde5d4054fe63f5aac7aa5c46e921f85
haozi <syntaxright@gmail.com>
2025-05-22 18:43:00 +0800

    feat: 🎸 v3.1.0 Add browser-storage.test and fix lsIdmp type deduction

---
+++ test/browser-storage.test.ts
@@ -0,0 +1,47 @@
+import storageWrap from 'idmp/browser-storage'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import idmp from '../src/index'
+
+const getInfo = async () => ({
+  name: 'John',
+  age: 30,
+  gender: 'male',
+})
+
+const lsIdmp = storageWrap(idmp, 'localStorage')
+
+const getInfoWithLsIdmp = () =>
+  lsIdmp('/api/your-info', getInfo, {
+    maxAge: 5 * 1000,
+  })
+
+describe('idmp/storage', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  it('It should print out the data from cache in localStorage', async () => {
+    const originalLog = console.log
+    const spy = vi.spyOn(console, 'log').mockImplementation((...args) => {
+      originalLog.apply(console, args)
+    })
+
+    await getInfoWithLsIdmp()
+
+    const promises: Array<ReturnType<typeof getInfoWithLsIdmp>> = []
+    for (let i = 0; i < 10; ++i) {
+      promises.push(getInfoWithLsIdmp())
+    }
+
+    await Promise.all(promises)
+
+    const calls = spy.mock.calls.flat()
+    const hitLogs = calls.filter(
+      (arg) => typeof arg === 'string' && arg.includes('from cache'),
+    )
+
+    expect(hitLogs.length).toBe(10)
+
+    spy.mockRestore()
+  })
+})
