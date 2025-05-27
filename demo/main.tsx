5d2339095693bf9ff450cc7ffc3f49efa7e2cc57
haozi <syntaxright@gmail.com>
2025-05-27 15:18:05 +0800

    docs: ✏️ Update demo

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import idmp from '../src'
 import App from './App'
+window['idmp'] = idmp
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
